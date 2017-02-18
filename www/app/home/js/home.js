6e0d34403414633ce27a51cea54a6642bacb5112
zerodennis <zogatowa32@gmail.com>
2017-02-18 21:07:39 +0800

    Made everything responsive

--- www/app/home/js/home.js
+++ www/app/home/js/home.js
@@ -1,5 +1,16 @@
 angular.module('bluehacks.homecontrollers', [])
 
 .controller('HomeCtrl', function($scope) {
-
+  function createTile(index) {
+    return {
+      url: '../img/' + index + '.png',
+      click: function(tile) {
+        // TODO: what happens when I click this tile
+      }
+    };
+  };
+  $scope.tiles = [];
+  for (var i = 1; i <= 17; ++i) {
+    $scope.tiles.push(createTile(i));
+  }
 });
