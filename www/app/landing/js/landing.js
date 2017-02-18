6c74d5240bb43dc365694a92758b572f29a5b527
Viktor Dela Cruz <viktormdelacruz@gmail.com>
2017-02-18 22:36:44 +0800

    Fixed sidemenu.

--- www/app/landing/js/landing.js
+++ www/app/landing/js/landing.js
@@ -5,15 +5,16 @@ angular.module('bluehacks.landingcontrollers', [])
 	$scope.data = {};
 
 	$scope.login = function(){
-		console.log("CLICKED LOGIN!");
+    $state.go('sidemenu.home');
+		/*console.log("CLICKED LOGIN!");
 		var query = "SELECT * FROM user WHERE username = ? AND password = ?"; //caution when using * if you want to be specific
       $cordovaSQLite.execute(db, query, [$scope.data.username, $scope.data.password]).then(function(result){
           if(result.rows.length > 0){
-            /*var query2 = "DELETE FROM current_user";
+            var query2 = "DELETE FROM current_user";
             $cordovaSQLite.execute(ebtrackerdb, query2);
             var query3 = "INSERT INTO current_user (current_user) VALUES (?)";
             $cordovaSQLite.execute(ebtrackerdb, query3, [$scope.user.username]);
-            console.log("SELECTED -> " + result.rows.item(0).firstname);*/
+            console.log("SELECTED -> " + result.rows.item(0).firstname);
             var alertPopup = $ionicPopup.alert({
               title: 'Welcome, ' + result.rows.item(0).username
             });
@@ -26,7 +27,7 @@ angular.module('bluehacks.landingcontrollers', [])
           }
       }, function(error){
           console.log(error);
-     })
+     })*/
 	}
 
 	$scope.register = function(){
