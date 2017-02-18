515be961114200eadd22cbb5a5ad3ad640247abd
Rico Tiongson <thericotiongson@gmail.com>
2017-02-19 05:53:12 +0800

    Initialized data loading model.

--- www/app/goal/js/goal.js
+++ www/app/goal/js/goal.js
@@ -2,12 +2,39 @@ angular.module('bluehacks.goalcontroller', [])
 
 .controller('GoalCtrl', function ($scope, $stateParams, DataService) {
 
-	$scope.data = {}
+	$scope.data = {};
+	$scope.tabs = {};
 
 	$scope.$on('$ionicView.beforeEnter', function() {
-		console.log("DATA IS", DataService.data.name)
-		$scope.data.name = DataService.data.name;
-		$scope.data.img = DataService.data.url;
+		angular.extend($scope.data, DataService.data);
+		// extend an empty set for loading later
+		angular.extend($scope.tabs, {
+			fbPage: [],
+			kickstarterCharities: [],
+			playStoreApps: [],
+			communityFeats: []
+		});
+		// load data in async
+		loadFbPages($scope.data, $scope.tabs.fbPage);
+		loadKickstarterCharities($scope.data, $scope.tabs.kickstarterCharities);
+		loadPlayStoreApps($scope.data, $scope.tabs.playStoreApps);
+		loadCommunityFeats($scope.data, $scope.tabs.communityFeats);
 	});
+
+	function loadFbPages(model, results) {
+		// TODO: async loading here to results
+	};
+
+	function loadKickstarterCharities(model, results) {
+		// TODO: async loading here to results
+	};
+
+	function loadPlayStoreApps(model, results) {
+		// TODO: async loading here to results
+	};
+
+	function loadCommunityFeats(model, results) {
+		// TODO: async loading here to results
+	};
 
 });
